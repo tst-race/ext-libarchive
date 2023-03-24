bc6859c1952231ef428ee12f65b103a2eef3d363
Kyle Treubig <kyle.treubig@twosixlabs.com>
2023-03-24 11:09:57 -0400

    Format build.py

--- build.py
+++ build.py
@@ -27,7 +27,13 @@ import race_ext_builder as builder
 
 def get_cli_arguments():
     """Parse command-line arguments to the script"""
-    parser = builder.get_arg_parser("libarchive", "3.3.2", 1, __file__, [builder.TARGET_ANDROID_x86_64, builder.TARGET_ANDROID_arm64_v8a])
+    parser = builder.get_arg_parser(
+        "libarchive",
+        "3.3.2",
+        1,
+        __file__,
+        [builder.TARGET_ANDROID_x86_64, builder.TARGET_ANDROID_arm64_v8a],
+    )
     return builder.normalize_args(parser.parse_args())
 
 
@@ -54,23 +60,33 @@ if __name__ == "__main__":
 
     logging.root.info("Configuring build")
     target = "x86_64-linux-android" if "x86" in args.target else "aarch64-linux-android"
-    builder.execute(args, [
-        "./configure",
-        f"--prefix=/",
-        f"--host={target}",
-        f"--target={target}"
-    ], cwd=source_dir, env=env)
+    builder.execute(
+        args,
+        ["./configure", f"--prefix=/", f"--host={target}", f"--target={target}"],
+        cwd=source_dir,
+        env=env,
+    )
 
     logging.root.info("Building")
-    builder.execute(args, [
-        "make",
-        "-j",
-        args.num_threads,
-    ], cwd=source_dir, env=env)
-    builder.execute(args, [
-        "make",
-        f"DESTDIR={args.install_dir}",
-        "install",
-    ], cwd=source_dir, env=env)
+    builder.execute(
+        args,
+        [
+            "make",
+            "-j",
+            args.num_threads,
+        ],
+        cwd=source_dir,
+        env=env,
+    )
+    builder.execute(
+        args,
+        [
+            "make",
+            f"DESTDIR={args.install_dir}",
+            "install",
+        ],
+        cwd=source_dir,
+        env=env,
+    )
 
     builder.create_package(args)
